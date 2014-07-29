d8474fca16ecc1d155d55ebd478ee89d47bb9651
Yuji Kamiya <yuji.kamiya@gree.net>
2014-07-29 09:51:53 +0900

    first commit

---
+++ src/System/Prefork/Main.hs
@@ -0,0 +1,9 @@
+module System.Prefork.Main where
+
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+multi :: Int -> Int -> Int
+multi x y = x * y
+
---
+++ test/test.hs
@@ -0,0 +1,5 @@
+import System.Prefork.Main
+
+main :: IO ()
+main = do
+  print $ add 1 2
